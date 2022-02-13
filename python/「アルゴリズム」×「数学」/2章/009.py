f4d9c322b9fe525c8e4e607b3093e73c563b1c7f
daisuke <daikon0313@yahoo.co.jp>
2022-02-13 20:04:10 +0900

    2022/02/13

--- python/「アルゴリズム」×「数学」/2章/009.py
+++ python/「アルゴリズム」×「数学」/2章/009.py
@@ -1,14 +1,27 @@
 N, S = list(map(int, input()))
 A = list(map(int, input().split()))
 
+#最初No
 ans = "No"
+#0から2^Nパターン
 for i in range(0, 1<<N):
 	partsum = 0
+	#Nまでのループ
 	for j in range(0, N):
-		if ( i & (1 << j)) != 0:
+		#
+		if (i & (1 << j)) != 0:
 			partsum += A[j]
 	if partsum == S:
 		ans = "Yes"
 		break
 
 print(ans)
+
+#>>> 1 << 2
+# 4
+#>>> 1 << 3
+# 8
+#>>> 1 << 4
+# 16
+#
+#andと&は同じ
