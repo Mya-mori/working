7ca75a6925a1185f204f06e6ef4a58805ba18a6d
daisuke <daikon0313@yahoo.co.jp>
2022-02-15 22:17:17 +0900

    2022/02/15

---
+++ python/「アルゴリズム」×「数学」/3章/問題/002.py
@@ -0,0 +1,12 @@
+def isprime(N):
+	LIMIT = int(N ** 0.5)
+	for i in range(2, LIMIT+1):
+		if N % i == 0:
+			return False
+	return True
+
+N = int(input())
+if isprime(N):
+	print("prime")
+else:
+	print("not prime")
