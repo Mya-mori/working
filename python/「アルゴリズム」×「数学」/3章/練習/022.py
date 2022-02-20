065142b7050030b47ec2aea71534d278d2408ad4
daisuke <daikon0313@yahoo.co.jp>
2022-02-20 22:57:52 +0900

    2022/02/20

---
+++ python/「アルゴリズム」×「数学」/3章/練習/022.py
@@ -0,0 +1,13 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+cnt = [0 for i in range(100000)]
+for i in range(N):
+	cnt[A[i]] += 1
+
+ans = 0
+for i in range(1, 50000):
+	ans += cnt[i] * cnt[100000 - i]
+ans += cnt[50000] * (cnt[50000] - i) // 2
+
+print(ans)
