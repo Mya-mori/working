2948fef4f92765bc21e41090c7e03471b91f0aef
daisuke <daikon0313@yahoo.co.jp>
2022-03-06 23:30:06 +0900

    2022/03/06

--- python/atcoder/ABC/242/C.py
+++ python/atcoder/ABC/242/C.py
@@ -1,8 +1,50 @@
+###############################################
+N, W = map(int, input().split())
+
+dp = [[0]*(W+1) for i in range(N+1)]
+
+for i in range(1, N+1):
+	wi, vi = map(int, input().split())
+
+	for w in range(W+1):
+		if w - wi < 0:
+			dp[i][w] = dp[i - 1][w]
+		else:
+			dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - wi] + vi)
+
+print(dp[N][W])
+###############################################
+
+#あまりの定義
+mod = 998244353
+
+#入力
 N = int(input())
 
-#1111, 1212, 1234, 1232などが該当する
+#初期値
+dp = [[0]*10 for i in range(N+1)]
 
-count = 0
-
-#一桁のうち取りうる値
+#1桁の時を埋める
 for i in range(1, 10):
+	dp[1][i] = 1
+
+#小さい順に埋めていく
+for d in range(2, N+1):
+	#i=1~9
+	for i in range(1, 10):
+		#2<=d, i=1
+		if i == 1:
+			dp[d][i] = dp[d - 1][i] +dp[d - 1][i + 1]
+		
+		#2<=d, 2<i<9
+		elif 2 <= i <= 8:
+			dp[d][i] = dp[d - 1][i - 1] + dp[d - 1][i] + dp[d - 1][i + 1]
+		
+		#2<=d, i=9
+		else:
+			dp[d][i] = dp[d - 1][i - 1] + dp[d - 1][i]
+
+		dp[d][i] %= mod
+print(sum(dp[1]))
+ans = sum(dp[N]) % mod
+print(ans)
