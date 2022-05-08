929a0617c3887dd0de54f27ce87b27445edb3524
daisuke <daikon0313@yahoo.co.jp>
2022-05-08 17:16:48 +0900

    2022/05/08

--- .history/python/algo/ソート/マージソート_20220508130844.py
+++
@@ -1,8 +0,0 @@
-N = int(input())
-A = list(map(int,input().split()))
-
-X = N//2
-#1
-L = A[:N//2]
-#2
-R = A[N//2:]
--- python/algo/ソート/マージソート.py
+++ python/algo/ソート/マージソート.py
@@ -1,26 +1,40 @@
 from collections import deque
 
-N = int(input())
-A = list(map(int,input().split()))
-
 def solve(A):
+    if len(A) <= 1:
+        return A
     #準備
-    X = len(A)//2
+    X = len(A) // 2
     #1
-    L = A[:N//2]
+    L = solve(A[:X])
     #2
-    R = A[N//2:]
+    R = solve(A[X:])
 
-    #3
-    L = solve(L)
-    R = solve(R)
-
-    #4
+    #3/4
     L += reversed(R)
-
     #5
     q = deque(L)
     B = []
-    print(q)
 
-print(solve(A))
+    while(q):
+        #e_firts <= e_last
+        if q[0] <= q[-1]:
+            f = q.popleft()
+            B.append(f)
+        #e_first > e_last
+        else:
+            e = q.pop()
+            B.append(e)
+
+    return B
+
+def main():
+    N = int(input())
+    A = list(map(int,input().split()))
+
+    ans = solve(A)
+
+    print(*ans)
+
+if __name__ == "__main__":
+    main()
