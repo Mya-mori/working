8787a12184da2966fd51cc05ed60d2cfad49dbb0
daisuke <daikon0313@yahoo.co.jp>
2022-05-08 12:10:31 +0900

    2022/05/08

--- python/algo/ソート/クイックソート.py
+++ python/algo/ソート/クイックソート.py
@@ -1,22 +1,22 @@
-N = int(input())
-A = list(map(int,input().split()))
-
 def quick_sort(array):
     # Xは(N/2)を基準とする
-    X = N // 2
-    if len(array) <= 1:
-        return array
-    # 
+    X = len(array) // 2
     L = []
     R = []
-    for i,x in enumerate(array):
-        if i!= X:
-            if array[i]<array[X]:
-                L.append(array[i])
-            else:
-                R.append(array[i])
-    L = quick_sort(L)
-    R = quick_sort(R)
-    return [*L,array[X],*R]
 
+    for i in range(len(array)):
+        if i == X:
+            continue
+        if array[i] < array[X]:
+            L.append(array[i])
+        else:
+            R.append(array[i])
+    if len(L) != 0:
+        L = quick_sort(L)
+    if len(R) != 0:
+        R = quick_sort(R)
+    return L + [array[X]] + R
+
+N = int(input())
+A = list(map(int,input().split()))
 print(*quick_sort(A))
