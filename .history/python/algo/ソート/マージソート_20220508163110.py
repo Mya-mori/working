e3bdf7d1321640d55d74d0cf149b2867f702f948
daisuke <daikon0313@yahoo.co.jp>
2022-05-08 16:36:57 +0900

    2022/05/08

---
+++ .history/python/algo/ソート/マージソート_20220508163110.py
@@ -0,0 +1,24 @@
+from audioop import reverse
+
+
+N = int(input())
+A = list(map(int,input().split()))
+
+def solve(A):
+    #準備
+    X = len(A)//2
+    #1
+    L = A[:N//2]
+    #2
+    R = A[N//2:]
+
+    #3
+    L = solve(L)
+    R = solve(R)
+
+    #4
+    L += reverse(R)
+    B = []
+
+    #5
+    
